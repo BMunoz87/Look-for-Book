446936c28b6c394363bfd07c9e7471b7eabab2ff
BMunoz87 <brianemunoz@outlook.com>
2021-08-04 14:11:26 -0500

    adding connection for Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://reading:rainbow1@clients.emzkl.mongodb.net/look-for-book?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
